31c24570bd204e66fd0ffe05b435b08e5dd99fe4
AngelGomez21 <aguilargomez21@gmail.com>
2025-11-26 13:32:29 -0600

    funcion dividir

--- Ejemplo.js
+++ Ejemplo.js
@@ -5,5 +5,9 @@ function Sumar(a,b){
 const Restar=(a,b)=> {
     return a-b;
 }
+function Dividir(a,b){
+    return a/b;
+}
 console.log(Restar(8,4));
 console.log(Sumar(2,4));
+console.log(Dividir(8,4));
